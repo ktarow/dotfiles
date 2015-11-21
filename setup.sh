614cf6dfef46c96b4e6be6e6edf3895a10520061
ktarow <ktarou569@gmail.com>
2015-11-21 17:18:36 +0900

    add: symbolic link

--- setup.sh
+++ setup.sh
@@ -7,4 +7,7 @@ fi
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 mkdir -p ~/.vim/indent
+ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/indent/ruby.vim $HOME/.vim/indent/ruby.vim
+ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc

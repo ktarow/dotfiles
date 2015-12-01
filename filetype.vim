c30733a57744023a992403b6d4195b7357a6a4a1
watanabeko <watanabeko@istyle.co.jp>
2015-12-01 13:14:57 +0900

    add: ftplugin

---
+++ filetype.vim
@@ -0,0 +1,6 @@
+augroup filetypedetect
+  au BufRead,BufNewFile *.rb setfiletype ruby
+  au BufRead,BufNewFile *.php setfiletype php
+  au BufRead,BufNewFile *.html setfiletype html
+  au BufRead,BufNewFile *.xml setfiletype xml
+augroup END
--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ if ! type git >/dev/null ; then
     exit 1
 fi
 
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
 
-mkdir -p ~/.vim/indent
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -sf $HOME/dotfiles/indent/ruby.vim $HOME/.vim/indent/ruby.vim
+ln -sf $HOME/dotfiles/ftplugin $HOME/.vim/ftplugin
+ln -sf $HOME/dotfiles/filetype.vim $HOME/.vim/filetype.vim
 ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
